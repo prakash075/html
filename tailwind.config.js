7be6a090591b15f0261f4e2580927dd40af59fbc
Bhupesh <myselfbhupesh00@gmail.com>
2022-09-03 11:07:20 +0530

    new project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        '220': '1000px',
+        '250': '1500px'
+      }
+    },
+  },
+  plugins: [],
+}
